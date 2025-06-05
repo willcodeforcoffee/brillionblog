0c56ee220582042d3578f0d2fda80dc11d09f142
Eric Hoff <eric@hoffsoftcorp.com>
2025-06-05 07:42:02 -0600

    Simple Users lists

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "home/index"
   resource :session
   resources :passwords, param: :token
+  resources :users, only: [ :index, :show ], param: :username
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
